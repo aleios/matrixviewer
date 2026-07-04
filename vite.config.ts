61fb4798940d4bd7f2055ba35cfc9b0dd1517c22
aleios <dvsaleios@gmail.com>
2026-07-04 19:17:39 +1000

    Add missing slash to root for vite

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
     vueDevTools(),
     tailwindcss()
   ],
-  base: '/maxtrixviewer',
+  base: '/maxtrixviewer/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
